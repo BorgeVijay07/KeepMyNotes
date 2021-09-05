820171c763af006ea8932969fc3722130125d6f3
BorgeVijay07 <borgevijay11@gmail.com>
2021-09-05 20:15:37 +0530

    Stored data into the database using mongoose model.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
 
-router.get('/', (req, res) => {
-    obj = {
-        a: 'vijay',
-        number: 90
-    }
-    res.json(obj)
+//Create a User using: POST "/api/auth" Not Required Auth
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 module.exports = router
